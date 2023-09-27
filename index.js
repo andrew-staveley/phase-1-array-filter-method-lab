41ac47f142b3f66f2424d2c95e775a64eb4ff8ce
Andrew <andrewstaveley14@gmail.com>
2023-09-27 18:30:43 -0400

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(array, name) {
+    return array.filter(nameMatcher);
+    function nameMatcher(element) {
+        if (element.toLowerCase() === name.toLowerCase()) {
+            return true
+        }
+    }
+}
+
+function fuzzyMatch(array, firstLetters) {
+    return array.filter(fuzzyMatcher);
+    function fuzzyMatcher(element) {
+        if (element.slice(0, 2) === firstLetters) {
+            return true
+        }
+    }
+}
+
+function matchName(array, name) {
+    return array.filter(nameMatcher2);
+    function nameMatcher2(element) {
+        if (element.name === name) {
+            return true
+        }
+    }
+}
+
